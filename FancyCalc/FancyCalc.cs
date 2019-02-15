17c4ec8f0d30a89e5288aba6981afdab783b87ba
alexeevaDaria <mari.marmeladkina@mail.ru>
2019-02-15 19:39:58 +0300

    1st change

--- FancyCalc/FancyCalc.cs
+++ FancyCalc/FancyCalc.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace FancyCalc
@@ -11,27 +12,54 @@ namespace FancyCalc
 
         public double Add(int a, int b)
         {
-            throw new NotImplementedException();
-            //return a + b;
+            return checked(a + b);
         }
 
 
         public double Subtract(int a, int b)
         {
             //throw new NotImplementedException();
-            return a - b;
+            return checked(a - b);
         }
 
 
         public double Multiply(int a, int b)
         {
-            return a * b;
+            return checked(a * b);
         }
 
+        public double Divide(int a, int b)
+        {
+            if (b == 0) throw new ArgumentNullException();
+            return checked(a / b);
+        }
         //generic calc method. usage: "10 + 20"  => result 30
         public double Culculate(string expression)
         {
-            throw new NotImplementedException();
+            string tmp = Regex.Replace(expression, "[^0-9.+*/-]+", "");
+            Match m = Regex.Match(tmp, "[+*/-]");
+
+            string[] str = Regex.Split(tmp, "[+*/-]");
+            int[] resArr = new int[2] { Convert.ToInt32(str[0]), Convert.ToInt32(str[1]) };
+            double res = 0;
+            char[] v = m.Value.ToCharArray();
+            switch (v[0])
+            {
+                case '+':
+                    res = Add(resArr[0],resArr[1]);
+                    break;
+                case '-':
+                    res = Subtract(resArr[0],resArr[1]);
+                    break;
+                case '/':
+                    res = Divide(resArr[0],resArr[1]);
+                    break;
+                case '*':
+                    res = Multiply(resArr[0], resArr[1]);
+                    break;
+            }
+
+            return res;
 
         }
     }
